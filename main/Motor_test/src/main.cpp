091005cd4cb428979eac0ed71746fba76ba20a1f
OgLocGreen <chri5_95@web.de>
2022-05-17 17:52:44 +0200

    Analog as communication Protocol
    working but cant get it working with i2c

--- main/Motor_test/src/main.cpp
+++ main/Motor_test/src/main.cpp
@@ -1,28 +1,88 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-/*
-  AnalogReadSerial
 
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+#define MD22ADDRESS 0xBC
+#define SOFTREG 0x07
+#define ACCELLREG 0x03
 
-  This example code is in the public domain.
 
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
-*/
+
+
+void SerialCommunication(int sensorValue,int channelA, int channelB, int speedValue);
+void setMode();
+void getSoftware();
+
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  Serial.write("Setup");
+  Wire.begin();
+
+  //getSoftware();                                              // Function that gets and prints software revision to screen
+  //setMode();                                                  // Function that sets mode to 2 and sets acceleration
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
+  int speedValue = 0;
   int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  int channelA = analogRead(A1);
+  int channelB = analogRead(A2);
+  
+  speedValue = map(sensorValue, 0, 1023, 0, 255);
+  speedValue = 128;
+
+  Wire.beginTransmission(MD22ADDRESS);                // Set first motor to speed 0
+  Wire.write(0x01);
+  Wire.write(0);
+  Wire.endTransmission();
+  delay(3000);                              
+
+  Wire.beginTransmission(0xBC);
+  Wire.write(0x01);
+  Wire.write(speedValue);
+  Wire.endTransmission();
+  delay(3000); 
+
+  SerialCommunication(sensorValue, channelA, channelB, speedValue);
 }
+
+// micosecond to mescure the ofset von the a nad b channel to mesure the speed
+
+void SerialCommunication(int sensorValue, int channelA, int channelB, int speedValue){
+  Serial.println("sensorValue");
+  Serial.println(sensorValue);
+
+  Serial.println("channelA");
+  Serial.println(channelA);
+
+  Serial.println("channelB");
+  Serial.println(channelB);
+
+  Serial.println("speedValue");
+  Serial.println(speedValue);
+}
+
+void getSoftware(){                                   // Reads and displays the software version of MD22
+  Wire.beginTransmission(MD22ADDRESS);                // Calles software register
+  Wire.write(SOFTREG);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(MD22ADDRESS, 1);                   // Requests one byte
+  while(Wire.available() < 1);                        // Wait for it to arrive
+  int software = Wire.read();                         // Get byte
+  Serial.println("MD22 Example   V:");
+  Serial.println(software, DEC);                        // Print byte to LCD03
+}
+
+void setMode(){
+  Wire.beginTransmission(MD22ADDRESS);                // Set a value of 255 to the acceleration register
+  Wire.write(ACCELLREG);
+  Wire.write(255);
+  Wire.endTransmission();
+}
+
