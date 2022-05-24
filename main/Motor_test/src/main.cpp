15574ead125dae8d4c2df580b3a9ccb928c8a099
OgLocGreen <chri5_95@web.de>
2022-05-24 19:24:40 +0200

    Motor is working

--- main/Motor_test/src/main.cpp
+++ main/Motor_test/src/main.cpp
@@ -2,27 +2,17 @@
 #include <Wire.h>
 
 
-#define MD22ADDRESS 0xBC
-#define SOFTREG 0x07
-#define ACCELLREG 0x03
-
-
-
-
 void SerialCommunication(int sensorValue,int channelA, int channelB, int speedValue);
-void setMode();
-void getSoftware();
 
+#define pwm_pin 3
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   Serial.write("Setup");
-  Wire.begin();
 
-  //getSoftware();                                              // Function that gets and prints software revision to screen
-  //setMode();                                                  // Function that sets mode to 2 and sets acceleration
+    pinMode(pwm_pin, OUTPUT);
 
 }
 
@@ -34,21 +24,11 @@ void loop() {
   int channelB = analogRead(A2);
   
   speedValue = map(sensorValue, 0, 1023, 0, 255);
-  speedValue = 128;
+  analogWrite(pwm_pin, speedValue);
 
-  Wire.beginTransmission(MD22ADDRESS);                // Set first motor to speed 0
-  Wire.write(0x01);
-  Wire.write(0);
-  Wire.endTransmission();
-  delay(3000);                              
-
-  Wire.beginTransmission(0xBC);
-  Wire.write(0x01);
-  Wire.write(speedValue);
-  Wire.endTransmission();
-  delay(3000); 
-
+                  
   SerialCommunication(sensorValue, channelA, channelB, speedValue);
+  delay(1000);
 }
 
 // micosecond to mescure the ofset von the a nad b channel to mesure the speed
@@ -67,22 +47,4 @@ void SerialCommunication(int sensorValue, int channelA, int channelB, int speedV
   Serial.println(speedValue);
 }
 
-void getSoftware(){                                   // Reads and displays the software version of MD22
-  Wire.beginTransmission(MD22ADDRESS);                // Calles software register
-  Wire.write(SOFTREG);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(MD22ADDRESS, 1);                   // Requests one byte
-  while(Wire.available() < 1);                        // Wait for it to arrive
-  int software = Wire.read();                         // Get byte
-  Serial.println("MD22 Example   V:");
-  Serial.println(software, DEC);                        // Print byte to LCD03
-}
-
-void setMode(){
-  Wire.beginTransmission(MD22ADDRESS);                // Set a value of 255 to the acceleration register
-  Wire.write(ACCELLREG);
-  Wire.write(255);
-  Wire.endTransmission();
-}
 
